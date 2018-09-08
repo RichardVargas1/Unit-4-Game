2d37c988c5fed744c3a492b6d6a77268fb94c550
RichardVargas1 <richard.vargas95@gmail.com>
2018-09-08 12:51:20 -0500

    Adding some more stuff.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,24 +1,48 @@
 
-var randomResult
-var win
-var loss
+var computerResult
+var previousNumber = 0
+var wins = 0
+var losses = 0
 
-userResult = Math.floor(Math.random() * 69 ) + 30;
+var resetAndStartTheGame = function () {
 
-$("#userResult").html('Your Total Score: ' + userResult);
+    computerResult = Math.floor(Math.random() * 69) + 30;
 
-// Creating a crystalBox variable within the for loop. I will format the boxes later via CSS.
-for(var i = 0; i < 4; i++) {
-    var randomNumber = Math.floor(Math.random() * 14) + 1;
-    // console.log(randomNumber);
-    var crystalBox = $("<div>");
-    crystalBox.attr({
-        "class": 'crystalBox',
-        "data-randomCrystalBoxNumber": randomNumber
-    });
-    $(".crystals").append(crystalBox);
+    $("#computerResult").html('Your number to match: ' + computerResult);
+
+    // Creating a crystalBox variable within the for loop. I will format the boxes later via CSS.
+    for (var i = 0; i < 4; i++) {
+        var randomNumber = Math.floor(Math.random() * 14) + 1;
+        // console.log(randomNumber);
+        var crystalBox = $("<div>");
+        // Setting values to my crystal boxes, by attributing them.
+            crystalBox.attr({
+                "class": 'crystalBox',
+                "data-randomCrystalBoxNumber": randomNumber
+        });
+            crystalBox.html(randomNumber);
+        $(".crystals").append(crystalBox);
+    }
 }
+
+resetAndStartTheGame();
 // jQuery code for clicking on on of my crystal boxes. This will communicate to the html file.
 $(".crystalBox").on('click', function () {
-    console.log($(this));
+
+    var num = parseInt($(this).attr('data-randomCrystalBoxNumber'));
+
+    previousNumber += num;
+
+    console.log(previousNumber);
+
+    if(previousNumber > computerResult){
+        losses--;
+        $("#losses").html(wins);
+        resetAndStartTheGame();
+    }
+    else if(previousNumber === computerResult){
+        wins++;
+        $("#wins").html(wins);
+        resetAndStartTheGame();
+    }
 });
