6f4e802e8c73a56582f1364cadf91e0bf6163bef
RichardVargas1 <richard.vargas95@gmail.com>
2018-09-07 22:52:05 -0500

    JavaScript Work, and other stuff.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,9 +2,23 @@
 var randomResult
 var win
 var loss
+
+userResult = Math.floor(Math.random() * 69 ) + 30;
+
+$("#userResult").html('Your Total Score: ' + userResult);
+
 // Creating a crystalBox variable within the for loop. I will format the boxes later via CSS.
 for(var i = 0; i < 4; i++) {
+    var randomNumber = Math.floor(Math.random() * 14) + 1;
+    // console.log(randomNumber);
     var crystalBox = $("<div>");
-    crystalBox.attr("class", 'crystalBox');
+    crystalBox.attr({
+        "class": 'crystalBox',
+        "data-randomCrystalBoxNumber": randomNumber
+    });
     $(".crystals").append(crystalBox);
 }
+// jQuery code for clicking on on of my crystal boxes. This will communicate to the html file.
+$(".crystalBox").on('click', function () {
+    console.log($(this));
+});
