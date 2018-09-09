0d40e6ceb4ba52ed5dc60831872c1cebb99e7900
RichardVargas1 <richard.vargas95@gmail.com>
2018-09-08 19:15:00 -0500

    Chnages in index, java and css.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,48 +1,57 @@
-
+// Current listed variables. Any other variables will be created within loops so to maintain order, for my own being.
 var computerResult
 var previousNumber = 0
-var wins = 0
-var losses = 0
+var win = 0
+var loss = 0
 
-var resetAndStartTheGame = function () {
+var resetAndStart = function () {
+
+    $(".crystalImages").empty();
 
     computerResult = Math.floor(Math.random() * 69) + 30;
 
-    $("#computerResult").html('Your number to match: ' + computerResult);
+    $("#goal").html('Desired Score: ' + computerResult);
 
     // Creating a crystalBox variable within the for loop. I will format the boxes later via CSS.
     for (var i = 0; i < 4; i++) {
-        var randomNumber = Math.floor(Math.random() * 14) + 1;
+        var random = Math.floor(Math.random() * 14) + 1;
         // console.log(randomNumber);
-        var crystalBox = $("<div>");
-        // Setting values to my crystal boxes, by attributing them.
-            crystalBox.attr({
-                "class": 'crystalBox',
-                "data-randomCrystalBoxNumber": randomNumber
-        });
-            crystalBox.html(randomNumber);
-        $(".crystals").append(crystalBox);
+        var crystal = $("<div>");
+            // Setting values to my crystal boxes, by attributing them.
+            crystal.attr({
+                    "class": 'crystal',
+                    "data-randomCrystalBoxNumber": random
+                });
+        $(".crystalImages").append(crystal);
     }
+    $("#previousNumber").html("Current Score: " + previousNumber);
 }
 
-resetAndStartTheGame();
+// This is going to be used to restart the game, win or loss.
+resetAndStart();
+
 // jQuery code for clicking on on of my crystal boxes. This will communicate to the html file.
-$(".crystalBox").on('click', function () {
+$(document).on('click', ".crystal", function () {
 
     var num = parseInt($(this).attr('data-randomCrystalBoxNumber'));
 
     previousNumber += num;
 
+    $("#previousNumber").html("Current Score: " + previousNumber);
+
     console.log(previousNumber);
 
-    if(previousNumber > computerResult){
-        losses--;
-        $("#losses").html(wins);
-        resetAndStartTheGame();
+    if (previousNumber > computerResult) {
+        loss--;
+        $("#loss").html("You loss: " + loss);
+        previousNumber = 0;
+        resetAndStart();
     }
-    else if(previousNumber === computerResult){
-        wins++;
-        $("#wins").html(wins);
-        resetAndStartTheGame();
+
+    else if (previousNumber === computerResult) {
+        win++;
+        $("#win").html("You win: " + win);
+        previousNumber = 0;
+        resetAndStart();
     }
 });
