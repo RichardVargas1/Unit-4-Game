9f0e6286f4e07bf9a74933e5c09f0de7ad898957
RichardVargas1 <richard.vargas95@gmail.com>
2018-09-09 20:28:42 -0500

    Finishing touches.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,6 +7,11 @@ var loss = 0
 var resetAndStart = function () {
 
     $(".crystalImages").empty();
+    var crystalPhotos = [
+        'https://t00.deviantart.net/mx0UNBntTF1oK_jAw_w6WtNjY94=/fit-in/700x350/filters:fixed_height(100,100):origin()/pre00/7db3/th/pre/f/2018/038/c/7/bejeweled_green_gem_by_ldinos-dc2fr2h.png',
+        'https://images-na.ssl-images-amazon.com/images/I/71NDLVvL-XL.png',
+        'https://pre00.deviantart.net/263e/th/pre/f/2018/039/7/5/bejeweled_blue_gem_by_ldinos-dc2j9bc.png',
+        'https://pre00.deviantart.net/9501/th/pre/f/2018/115/d/c/bejeweled_purple_gem_by_ldinos-dc9v7iu.png']
 
     computerResult = Math.floor(Math.random() * 69) + 30;
 
@@ -14,13 +19,17 @@ var resetAndStart = function () {
 
     // Creating a crystalBox variable within the for loop. I will format the boxes later via CSS.
     for (var i = 0; i < 4; i++) {
-        var random = Math.floor(Math.random() * 14) + 1;
+        var randomNumero = Math.floor(Math.random() * 14) + 1;
         // console.log(randomNumber);
         var crystal = $("<div>");
             // Setting values to my crystal boxes, by attributing them.
             crystal.attr({
                     "class": 'crystal',
-                    "data-randomCrystalBoxNumber": random
+                    "data-randomCrystalBoxNumber": randomNumero
+                });
+                crystal.css({
+                    "background-image": "url('" + crystalPhotos[i] + "')",
+                    "background-size": "cover"
                 });
         $(".crystalImages").append(crystal);
     }
